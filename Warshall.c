790b05d479f159fc2ff204dad4c40387c66f31a7
Chethan <86680859+ChethanKumar-K@users.noreply.github.com>
2022-08-17 18:38:42 +0530

    Create Warshall.c
    Used to find the transitive closure

---
+++ Warshall.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int n;
+int a[20][20];
+void warshall(int a[][20])
+{
+	for(int k=0;k<n;k++)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+				a[i][j]=a[i][j] || (a[i][k]&&a[k][j]);
+		}
+	}
+}
+
+int main()
+{
+	
+	printf("Enter the no. of vertices");
+	scanf("%d",&n);
+	
+	printf("Enter the adjacency matrix");
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			scanf("%d",&a[i][j]);
+			
+		}
+	}
+	warshall(a);
+	printf("Transitive closure is");
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			printf("%d ",a[i][j]);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
